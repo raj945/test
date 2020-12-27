a7531819d742eb368fa1cdad06b1b13f5ee3f709
raj945 <68217979+raj945@users.noreply.github.com>
2020-12-27 14:42:39 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Icon, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section, SocialMedia } from "@quarkly/components";
+import { MdFace } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Icon category="md" icon={MdFace} />
+		<Section>
+			<Text margin="0px 0 24px 0" text-align="center" font="--headline2" md-font="--headline3">
+				NEW RAJ RATA SAREE
+			</Text>
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--base"
+				font-weight="700"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px" />
+			</Menu>
+		</Section>
+		<SocialMedia
+			facebook="https://www.facebook.com/quarklyapp/"
+			twitter="https://twitter.com/quarklyapp"
+			youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+			vkontakte="https://vk.com/quarklyapp"
+			telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+		/>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe8406cd84ef0001e94f182",
+				"5fe8406cd84ef0001e94f184"
+			]
+		},
+		"5fe8406cd84ef0001e94f182": {
+			"id": "5fe8406cd84ef0001e94f182",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe8406cd84ef0001e94f184": {
+			"id": "5fe8406cd84ef0001e94f184",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
